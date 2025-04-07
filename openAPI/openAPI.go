6037d39bc78be7ba213e8d00b4bbb36ffad184e9
Tiny_Murky <murky0830@gmail.com>
2025-04-08 00:46:25 +0800

    GetOneAPIByPath

--- openAPI/openAPI.go
+++ openAPI/openAPI.go
@@ -1,8 +1,12 @@
 package openapi
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/pb33f/libopenapi"
 	v3high "github.com/pb33f/libopenapi/datamodel/high/v3"
+	v3 "github.com/pb33f/libopenapi/datamodel/low/v3"
 )
 
 // OpenAPI contain all field that need to be used in openapi package
@@ -59,4 +63,46 @@ func (o *OpenAPI) ListAllAPIFromDocument() []SimplifyAPI {
 	}
 
 	return simplifyAPIs
+}
+
+func (o *OpenAPI) GetOneAPIByPath(path string, method string) (*v3high.Operation, error) {
+	methodLower := strings.ToLower(method)
+
+	allowMethod := map[string]bool{
+		v3.GetLabel:     true,
+		v3.PutLabel:     true,
+		v3.PostLabel:    true,
+		v3.DeleteLabel:  true,
+		v3.OptionsLabel: true,
+		v3.HeadLabel:    true,
+		v3.PatchLabel:   true,
+		v3.TraceLabel:   true,
+	}
+
+	if !allowMethod[methodLower] {
+		return nil, fmt.Errorf("Method %g is not allowed", method)
+	}
+
+	var pathItem *v3high.PathItem
+
+	for pathPairs := o.docModelV3.Model.Paths.PathItems.First(); pathPairs != nil; pathPairs = pathPairs.Next() {
+		pathName := pathPairs.Key()
+		if pathName == path {
+			pathItem = pathPairs.Value()
+			break
+		}
+	}
+
+	if pathItem == nil {
+		return nil, fmt.Errorf("Url path %q was not founded in OpenAPI file", path)
+	}
+
+	for option := pathItem.GetOperations().First(); option != nil; option = option.Next() {
+		if option.Key() == methodLower {
+			return option.Value(), nil
+		}
+	}
+
+	return nil, fmt.Errorf("Method %q not found in Url Path %q", methodLower, path)
+
 }
